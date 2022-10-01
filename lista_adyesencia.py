205ed5c578584a96918901bd9fe53e21fcd854f0
Adalberto Emmanuel Rojas <mamueljr@gmail.com>
2022-09-30 19:34:40 -0600

    Update lista_adyesencia.py

--- lista_adyesencia.py
+++ lista_adyesencia.py
@@ -4,21 +4,23 @@
 # Analisis y diseño de algoritmos MIC
 # Definir funcion para insertar vertice a la lista de adyesencia
 
-def insertar_vertice(adj, u, v):
+def insertar_vertice(lista, u, v):
 
     # Insertar vertice v a vertice u
-    adj[u].append(v)
+    lista[u].append(v)
     return
+'''
+ Definir funcion para imprimir la lista de adyesencia,
+ recibe como argumento el numero de vertices V
+'''
 
-# Definir funcion para imprimir la lista de adyesencia, resive como argumento el numero de vertices V
-
-
-def imprimir_lista(adj, V):
+def imprimir_lista(lista, V):
 
     for i in range(V):
+        #Uso el parametro end para no dar el salto de linea y adjuntalo con los nodos
         print(i, end='')
 
-        for j in adj[i]:
+        for j in lista[i]:
             print(' -> ' + str(j), end='')
 
         print()
@@ -31,28 +33,28 @@ if __name__ == '__main__':
     # numero de vertices
     V = 5
 
-    listaDeAdjesencia = [[] for i in range(V)]
+    listaDelistaesencia = [[] for i in range(V)]
 
     # Insertar aristas en esta ocacion dejare los Vertices y aristas aqui en el codigo,
     # Para futuras trareas agrego codigo pare pedir numero de nodos y sus aristas
-    insertar_vertice(listaDeAdjesencia, 0, 1)
-    insertar_vertice(listaDeAdjesencia, 0, 4)
-    insertar_vertice(listaDeAdjesencia, 1, 0)
-    insertar_vertice(listaDeAdjesencia, 1, 2)
-    insertar_vertice(listaDeAdjesencia, 1, 3)
-    insertar_vertice(listaDeAdjesencia, 1, 4)
-    insertar_vertice(listaDeAdjesencia, 2, 1)
-    insertar_vertice(listaDeAdjesencia, 2, 3)
-    insertar_vertice(listaDeAdjesencia, 3, 1)
-    insertar_vertice(listaDeAdjesencia, 3, 2)
-    insertar_vertice(listaDeAdjesencia, 3, 4)
-    insertar_vertice(listaDeAdjesencia, 4, 0)
-    insertar_vertice(listaDeAdjesencia, 4, 1)
-    insertar_vertice(listaDeAdjesencia, 4, 3)
+    insertar_vertice(listaDelistaesencia, 0, 1)
+    insertar_vertice(listaDelistaesencia, 0, 4)
+    insertar_vertice(listaDelistaesencia, 1, 0)
+    insertar_vertice(listaDelistaesencia, 1, 2)
+    insertar_vertice(listaDelistaesencia, 1, 3)
+    insertar_vertice(listaDelistaesencia, 1, 4)
+    insertar_vertice(listaDelistaesencia, 2, 1)
+    insertar_vertice(listaDelistaesencia, 2, 3)
+    insertar_vertice(listaDelistaesencia, 3, 1)
+    insertar_vertice(listaDelistaesencia, 3, 2)
+    insertar_vertice(listaDelistaesencia, 3, 4)
+    insertar_vertice(listaDelistaesencia, 4, 0)
+    insertar_vertice(listaDelistaesencia, 4, 1)
+    insertar_vertice(listaDelistaesencia, 4, 3)
 
     # Imprimir la lista de Adyesencia
     print("---------------------------------------------------------")
     print("-------------- Lista de Adyesencia ----------------------")
     print("---------------------------------------------------------")
     print("NODO -vertice-> NODO ...")
-    imprimir_lista(listaDeAdjesencia, V)
+    imprimir_lista(listaDelistaesencia, V)
